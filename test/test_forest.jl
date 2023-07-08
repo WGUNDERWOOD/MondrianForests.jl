ab7f3d4eea5b9ca30552442e76e8509aac6dc727
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-07-07 21:52:08 -0400

    test

--- test/test_forest.jl
+++ test/test_forest.jl
@@ -1,6 +1,6 @@
 @testset verbose = true "Forests" begin
 
-    @testset verbose = true "Constants" begin
+    @testset verbose = true "Constant" begin
         n = 50
         lambda = 5.0
         n_trees = 20
@@ -13,10 +13,26 @@
             Y = [2.0 for _ in 1:n]
             forest = MondrianForest(lambda, n_trees, x_evals, debias_order,
                                     significance_level, X, Y)
+            @suppress show(forest)
             @test all(forest.mu_hat .== 2)
         end
     end
 
+    @testset verbose = true "Linear" begin
+        n = 100
+        lambda = 10.0
+        n_trees = 100
+        debias_order = 0
+        significance_level = 0.05
+        for d in 1:3
+            X = [ntuple(i -> rand(), d) for _ in 1:n]
+            x_evals = [ntuple(i -> 0.5, d)]
+            Y = sum.(X)
+            forest = MondrianForest(lambda, n_trees, x_evals, debias_order,
+                                    significance_level, X, Y)
+            @test isapprox(forest.mu_hat[], 0.5 * d, rtol=0.1)
+        end
+    end
 
     #for d in 1:2
         #lambda = 10.0
