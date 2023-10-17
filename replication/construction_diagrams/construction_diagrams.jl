5789ab7d05e21c52c41fb3c8d62c6c89cf8149ce
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-10-17 16:29:01 +0100

    construction diagrams

--- replication/construction_diagrams/construction_diagrams.jl
+++ replication/construction_diagrams/construction_diagrams.jl
@@ -225,35 +225,6 @@ function plot_mondrian_tree(partition)
     return (fig, ax)
 end
 
-# construct a good tree
-d = 2
-lambda = 2.0
-Random.seed!(0)
-min_vol = 0.0
-n_cells = 1
-while min_vol < 0.2 || n_cells != 4
-    global tree = MondrianTree(d, lambda)
-    global cells = MondrianForests.get_cells(tree)
-    global min_vol = minimum(MondrianForests.get_volume(c) for c in cells)
-    global n_cells = length(cells)
-end
-
-# get locations of tree nodes for diagram
-info = get_tree_info(tree)
-ids = [i[1] for i in info]
-xs = get_horizontal_value.(ids)
-xs = invperm(sortperm(xs)) / 3
-x_locs = Dict()
-for i in 1:length(ids)
-    x_locs[ids[i]] = xs[i]
-end
-
-# calculate the current and terminal nodes
-partitions = [Dict("time" => 0.0,
-                   "tree" => MondrianForests.restrict(tree, 0.0),
-                   "current" => nothing,
-                   "terminals" => [])]
-
 function update_partitions(partitions, tree)
     p = partitions[end]
     info = get_tree_info(p["tree"])
@@ -303,6 +274,35 @@ function update_partitions(partitions, tree)
     return [partitions; [new_partition]]
 end
 
+# construct a good tree
+d = 2
+lambda = 2.0
+Random.seed!(0)
+min_vol = 0.0
+n_cells = 1
+while min_vol < 0.2 || n_cells != 4
+    global tree = MondrianTree(d, lambda)
+    global cells = MondrianForests.get_cells(tree)
+    global min_vol = minimum(MondrianForests.get_volume(c) for c in cells)
+    global n_cells = length(cells)
+end
+
+# get locations of tree nodes for diagram
+info = get_tree_info(tree)
+ids = [i[1] for i in info]
+xs = get_horizontal_value.(ids)
+xs = invperm(sortperm(xs)) / 3
+x_locs = Dict()
+for i in 1:length(ids)
+    x_locs[ids[i]] = xs[i]
+end
+
+# calculate the current and terminal nodes
+partitions = [Dict("time" => 0.0,
+                   "tree" => MondrianForests.restrict(tree, 0.0),
+                   "current" => nothing,
+                   "terminals" => [])]
+
 info = get_tree_info(tree)
 times = [i[2] for i in info]
 for rep in 1:11
@@ -316,8 +316,7 @@ for i in 1:length(partitions)
     println(i)
     partition = partitions[i]
     global (fig, ax) = plot_mondrian_tree(partition)
-    plt.savefig("replication/mondrian_process/mondrian_tree_$(i).png", dpi=dpi)
-    #bbox_inches="tight", pad_inches=0.01)
+    plt.savefig("replication/construction_diagrams/mondrian_tree_$(i).png", dpi=dpi)
     plt.close("all")
 end
 
@@ -327,227 +326,6 @@ for i in 1:length(partitions)
     println(i)
     partition = partitions[i]
     global (fig, ax) = plot_mondrian_process(partition)
-    plt.savefig("replication/mondrian_process/mondrian_process_$(i).png", dpi=dpi)
+    plt.savefig("replication/construction_diagrams/mondrian_process_$(i).png", dpi=dpi)
     plt.close("all")
 end
-
-function plot_theorem_restriction(tree, cell)
-    splits = get_splits(tree)
-    (fig, ax) = plt.subplots(figsize=(2.2, 2.2))
-
-    # plot root cell and extra cell
-    colors = ["k", "k"]
-    styles = ["solid", "dashed"]
-    lw = 0.9
-    for i in 1:2
-        c = [tree.cell, cell][i]
-        color = colors[i]
-        style = styles[i]
-        (l1, l2) = c.lower
-        (u1, u2) = c.upper
-        plt.plot([l1, l1], [l2, u2], color=color, lw=lw, linestyle=style)
-        plt.plot([u1, u1], [l2, u2], color=color, lw=lw, linestyle=style)
-        plt.plot([l1, u1], [l2, l2], color=color, lw=lw, linestyle=style)
-        plt.plot([u1, l1], [u2, u2], color=color, lw=lw, linestyle=style)
-    end
-
-    # annotate extra cell
-    plt.text(0.39, 0.33, "\$C\$", fontsize=10)
-
-    # plot splits
-    for split in splits
-        x1s = [point[1] for point in split]
-        x2s = [point[2] for point in split]
-        plt.plot(x1s, x2s, ms=0, color="k", lw=lw)
-    end
-
-    # format plot
-    plt.ylim([-0.05, 1.01])
-    plt.xticks([0, 1])
-    plt.yticks([0, 1])
-    plt.xlabel("\$x_1\$")
-    plt.ylabel("\$x_2\$")
-    ax.xaxis.set_label_coords(0.5, -0.04)
-    ax.yaxis.set_label_coords(-0.04, 0.5)
-    ax.tick_params(color="w", direction="in", pad=0)
-    ax.set_aspect("equal")
-    for side in ["bottom", "top", "left", "right"]
-        ax.spines[side].set_color("#FFFFFF00")
-    end
-    plt.tight_layout()
-    return (fig, ax)
-end
-
-# restriction theorem plot
-println("plotting restriction theorem")
-tree = MondrianForests.restrict(tree, 1.5)
-cell = MondrianCell((0.5, 0.3), (0.9, 0.85))
-(fig, ax) = plot_theorem_restriction(tree, cell)
-plt.savefig("replication/mondrian_process/theorem_restriction.png", dpi=dpi)
-plt.close("all")
-
-function plot_theorem_distribution(tree, point)
-    splits = get_splits(tree)
-    (fig, ax) = plt.subplots(figsize=(2.2, 2.2))
-
-    # plot root cell
-    lw = 0.9
-    (l1, l2) = tree.cell.lower
-    (u1, u2) = tree.cell.upper
-    plt.plot([l1, l1], [l2, u2], color="k", lw=lw)
-    plt.plot([u1, u1], [l2, u2], color="k", lw=lw)
-    plt.plot([l1, u1], [l2, l2], color="k", lw=lw)
-    plt.plot([u1, l1], [u2, u2], color="k", lw=lw)
-
-    # get cell containing point
-    cells = MondrianForests.get_cells(tree)
-    cell = [c for c in cells if MondrianForests.is_in(point, c)][]
-
-    # plot point and distribution
-    e1 = 0.03
-    e2 = 0.1
-    plt.arrow(point[1] + e1, point[2], cell.upper[1] - point[1] - e2, 0,
-              head_width=0.02, head_length=0.03, ec="k", fc="k")
-    plt.arrow(point[1] - e1, point[2], cell.lower[1] - point[1] + e2, 0,
-              head_width=0.02, head_length=0.03, ec="k", fc="k")
-    plt.arrow(point[1], point[2] + e1, 0, cell.upper[2] - point[2] - e2,
-              head_width=0.02, head_length=0.03, ec="k", fc="k")
-    plt.arrow(point[1], point[2] - e1, 0, cell.lower[2] - point[2] + e2,
-              head_width=0.02, head_length=0.03, ec="k", fc="k")
-    plt.scatter(point[1], point[2], color="k", s=2)
-
-    # annotate point and arrows
-    plt.text(0.44, 0.28, "\$x\$", fontsize=9)
-    plt.text(0.07, 0.29, "\$x_1\$", fontsize=9)
-    plt.text(0.28, 0.04, "\$x_2\$", fontsize=9)
-    plt.text(0.61, 0.15, "\$E_{12}\$", fontsize=9)
-    plt.text(0.44, 0.65, "\$E_{22}\$", fontsize=9)
-
-    # plot splits
-    for split in splits
-        x1s = [point[1] for point in split]
-        x2s = [point[2] for point in split]
-        plt.plot(x1s, x2s, ms=0, color="k", lw=lw)
-    end
-
-    # format plot
-    plt.ylim([-0.05, 1.01])
-    plt.xticks([0, 1])
-    plt.yticks([0, 1])
-    plt.xlabel("\$x_1\$")
-    plt.ylabel("\$x_2\$")
-    ax.xaxis.set_label_coords(0.5, -0.04)
-    ax.yaxis.set_label_coords(-0.04, 0.5)
-    ax.tick_params(color="w", direction="in", pad=0)
-    ax.set_aspect("equal")
-    for side in ["bottom", "top", "left", "right"]
-        ax.spines[side].set_color("#FFFFFF00")
-    end
-    plt.tight_layout()
-    return (fig, ax)
-end
-
-# distribution theorem plot
-println("plotting distribution theorem")
-point = (0.4, 0.25)
-(fig, ax) = plot_theorem_distribution(tree, point)
-plt.savefig("replication/mondrian_process/theorem_distribution.png", dpi=dpi)
-plt.close("all")
-
-function plot_piet_mondrian(tree)
-    splits = get_splits(tree)
-    (fig, ax) = plt.subplots(figsize=(2.2, 2.2))
-
-    # plot root cell
-    lw = 0.9
-    (l1, l2) = tree.cell.lower
-    (u1, u2) = tree.cell.upper
-    plt.plot([l1, l1], [l2, u2], color="k", lw=lw)
-    plt.plot([u1, u1], [l2, u2], color="k", lw=lw)
-    plt.plot([l1, u1], [l2, l2], color="k", lw=lw)
-    plt.plot([u1, l1], [u2, u2], color="k", lw=lw)
-
-    # plot splits
-    for split in splits
-        x1s = [point[1] for point in split]
-        x2s = [point[2] for point in split]
-        plt.plot(x1s, x2s, ms=0, color="k", lw=lw)
-    end
-
-    # format plot
-    plt.ylim([-0.01, 1.01])
-    plt.xticks([])
-    plt.yticks([])
-    #plt.xlabel("\$x_1\$")
-    #plt.ylabel("\$x_2\$")
-    #ax.xaxis.set_label_coords(0.5, -0.04)
-    #ax.yaxis.set_label_coords(-0.04, 0.5)
-    ax.tick_params(color="w", direction="in", pad=0)
-    ax.set_aspect("equal")
-    for side in ["bottom", "top", "left", "right"]
-        ax.spines[side].set_color("#FFFFFF00")
-    end
-    plt.tight_layout()
-    return (fig, ax)
-end
-
-# piet plot
-println("plotting piet")
-lambda = 4.0
-min_vol = 0.0
-best_min_vol = 0.0
-while min_vol < 0.01 || n_cells < 10
-    global tree = MondrianTree(d, lambda)
-    global cells = MondrianForests.get_cells(tree)
-    global min_vol = minimum(MondrianForests.get_volume(c) for c in cells)
-    global n_cells = length(cells)
-    if min_vol > best_min_vol
-        global best_min_vol = min_vol
-    end
-end
-(fig, ax) = plot_piet_mondrian(tree)
-plt.savefig("replication/mondrian_process/piet_mondrian.png", dpi=dpi,
-            bbox_inches="tight", pad_inches=-0.008)
-plt.close("all")
-
-function plot_first_order_bias(tree)
-    lw = 0.9
-    splits = get_splits(tree)
-    splits = [s[1][1] for s in splits]
-    splits = [0; splits; 1]
-    (fig, ax) = plt.subplots(figsize=(2.2, 2.2))
-
-    for split in splits
-        plt.plot([split, split], [0, 1], color="k",
-                 linestyle="dashed", lw=lw)
-    end
-
-    for i in 1:(length(splits) - 1)
-        y = (splits[i] + splits[i + 1]) / 2
-        plt.plot([splits[i], splits[i + 1]], [y, y], color="k", lw=lw)
-    end
-    plt.plot([0, 1], [0, 1], color="k", lw=lw)
-
-    # label cell width
-    plt.text(0.73, 0.08, "\$\\sim \\frac{1}{\\lambda}\$", fontsize=10)
-    plt.arrow(0.9, 0.2, -0.23, 0, head_width=0.02,
-              head_length=0.03, ec="k", fc="k")
-    plt.arrow(0.8, 0.2, 0.13, 0, head_width=0.018,
-              head_length=0.03, ec="k", fc="k")
-
-    # format plot
-    plt.xticks([0, 1])
-    plt.yticks([0, 1])
-    plt.xlabel("\$x\$")
-    plt.ylabel("\$y\$")
-    ax.xaxis.set_label_coords(0.5, -0.04)
-    ax.yaxis.set_label_coords(-0.04, 0.5)
-    plt.tight_layout()
-    return (fig, ax)
-end
-
-Random.seed!(5)
-tree = MondrianTree(1, 2.0)
-(fig, ax) = plot_first_order_bias(tree)
-plt.savefig("replication/mondrian_process/first_order_bias.png", dpi=dpi)
-plt.close("all")
