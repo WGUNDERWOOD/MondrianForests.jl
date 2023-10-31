f25d790425cf5259452f93743b4a6943a337d263
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-10-30 21:54:41 -0400

    rename

--- make.jl
+++ make.jl
@@ -2,5 +2,21 @@ using JuliaFormatter
 using MondrianForests
 using Pkg
 
+# format
 format(MondrianForests)
+
+# test
 Pkg.test()
+
+# docs
+include("docs/make.jl")
+
+# replication
+include("replication/construction_diagrams/construction_diagrams.jl")
+include("replication/logo/logo.jl")
+include("replication/partition_plots/partition_plots.jl")
+include("replication/piet_diagram/piet_diagram.jl")
+include("replication/readme_examples/readme_examples.jl")
+include("replication/theorem_diagrams/theorem_diagrams.jl")
+include("replication/weather/weather.jl")
+include("replication/weather_cv/weather_cv.jl")
