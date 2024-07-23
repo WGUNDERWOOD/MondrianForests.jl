a95a6eaabbfb64981f7af48f6f0fd99812c38a74
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-07-23 10:30:36 +0100

    more work on replication

--- replication/debiasing/debiasing.jl
+++ replication/debiasing/debiasing.jl
@@ -26,6 +26,8 @@ mutable struct Experiment
     lambda_target::LambdaTarget
     lambda_method::LambdaMethod
     lambda_multipliers::Vector{Float64}
+    lambda_candidates::Vector{Float64}
+    n_subsample::Int
 
     # distribution
     d::Int
@@ -45,6 +47,8 @@ mutable struct Experiment
     coverage::Float64
     average_width::Float64
     lambda::Float64
+    sd_theory::Float64
+    bias_theory::Float64
 end
 
 function Experiment(
@@ -52,6 +56,8 @@ function Experiment(
         lambda_target::LambdaTarget,
         lambda_method::LambdaMethod,
         lambda_multipliers::Vector{Float64},
+        lambda_candidates::Vector{Float64},
+        n_subsample::Int,
         d::Int,
         n::Int,
         B_estimator::Int,
@@ -67,6 +73,8 @@ function Experiment(
                lambda_target,
                lambda_method,
                lambda_multipliers,
+               lambda_candidates,
+               n_subsample,
                d,
                n,
                B_estimator,
@@ -82,6 +90,8 @@ function Experiment(
                NaN,
                NaN,
                NaN,
+               NaN,
+               NaN
               )
 end
 
@@ -90,19 +100,22 @@ function run_first_block()
     lambda_target = rmse::LambdaTarget
     lambda_methods = instances(LambdaMethod)
     lambda_multipliers = [0.9, 1.0]
+    lambda_candidates = [4.0, 5.0]
+    n_subsample = 20
     d = 1
     n = 1000
     B_estimator = 200
     B_lifetime = 200
     x_evals = [ntuple(j -> 0.5, d)]
     X_dist = Uniform(0, 1)
-    mu = (x -> sum(x.^2))
-    sigma = 0.01
+    mu = (x -> sum(sin.(x)))
+    sigma = 0.1
     eps_dist = Normal(0, sigma)
     experiment = Experiment(J_estimator, lambda_target,
                             lambda_methods[1], lambda_multipliers,
-                            d, n, B_estimator, B_lifetime,
-                            x_evals, X_dist, mu, eps_dist)
+                            lambda_candidates, n_subsample,
+                            d, n, B_estimator,
+                            B_lifetime, x_evals, X_dist, mu, eps_dist)
     run(experiment)
 end
 
@@ -114,15 +127,36 @@ function get_J_lifetime(experiment)
     end
 end
 
-function select_lifetime(experiment)
-    X = experiment.X
-    Y = experiment.Y
+function get_theory(experiment::Experiment)
+    n = experiment.n
+    d = experiment.d
+    lambda = experiment.lambda
+    x_evals = experiment.x_evals
+    sigma2 = var(experiment.eps_dist)
+    if experiment.J_estimator == 0
+        C0 = (4 - 4 * log(2)) / 3
+        experiment.sd_theory = sqrt(lambda^d * sigma2 * C0^d / n)
+        # TODO calculate sin bias
+        experiment.bias_theory = - sum(sin.(x_evals[])) / (2 * lambda^2)
+    elseif experiment.J_estimator == 1
+        C1 = (4/3 - 4*log(2)/3)
+        C2 = (2 - 2*log(2))
+        C3 = (5/3 - log(5/2) - 3*log(5/3)/2)
+        C_all = 16/5 * C1^d + 81/25 * C2^d - 72/5 * C3^d
+        experiment.sd_theory = sqrt(lambda^d * sigma2 * C_all / n)
+        # TODO calculate sin bias
+        experiment.bias_theory = d / lambda^2
+    end
+end
+
+function select_lifetime(X, Y, experiment)
     J = experiment.J_lifetime
-    lambdas = experiment.lambdas
+    lambda_candidates = experiment.lambda_candidates
     B = experiment.B_lifetime
     n_subsample = experiment.n_subsample
     if experiment.lambda_method == optimal::LambdaMethod
         # TODO
+        return 10.0
     elseif experiment.lambda_method == polynomial::LambdaMethod
         return select_lifetime_polynomial(X, Y, J)
     elseif experiment.lambda_method == gcv::LambdaMethod
@@ -131,10 +165,11 @@ function select_lifetime(experiment)
 end
 
 function run(experiment::Experiment)
-    n_rep = 50
+    n_rep = 200
     n = experiment.n
     d = experiment.d
     x_evals = experiment.x_evals
+    mu = experiment.mu
     experiment.J_lifetime = get_J_lifetime(experiment)
     mse = 0.0
     bias = 0.0
@@ -144,15 +179,16 @@ function run(experiment::Experiment)
     for rep in 1:n_rep 
         println(rep)
         X = [ntuple(j -> rand(experiment.X_dist), d) for i in 1:n]
-        Y = [X[i][1]^2 + rand(experiment.eps_dist) for i in 1:n]
-        # TODO
-        lambda = select_lifetime_polynomial(X, Y, experiment.J_lifetime)
+        Y = [mu(X[i]) + rand(experiment.eps_dist) for i in 1:n]
+        lambda = select_lifetime(X, Y, experiment)
         forest = DebiasedMondrianForest(lambda, experiment.B_estimator,
                                         x_evals,
                                         experiment.J_estimator, X, Y, true)
+        #println(forest.debias_scaling)
+        #println(forest.debias_coeffs)
         ci = forest.confidence_band
-        mse += (forest.mu_hat[] - d/4)^2 / n_rep
-        bias += (forest.mu_hat[] - d/4) / n_rep
+        mse += (forest.mu_hat[] - mu(x_evals[]))^2 / n_rep
+        bias += (forest.mu_hat[] - mu(x_evals[])) / n_rep
         coverage += (ci[][1] <= 0 <= ci[][2]) / n_rep
         average_width += (ci[][2] - ci[][1]) / n_rep
         average_lambda += lambda / n_rep
@@ -164,6 +200,7 @@ function run(experiment::Experiment)
     experiment.coverage = coverage
     experiment.average_width = average_width
     experiment.lambda = average_lambda
+    get_theory(experiment)
     #show(experiment)
 
     for f in fieldnames(Experiment)
@@ -171,9 +208,6 @@ function run(experiment::Experiment)
         println("$f: $v")
     end
 
-    sigma2 = var(experiment.eps_dist)
-    println("theory sd: ", sqrt(average_lambda^d * sigma2 * 0.4091^d / n) )
-    println("theory bias: ", d / average_lambda^2)
 end
 
 run_first_block()
